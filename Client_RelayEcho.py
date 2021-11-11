a2a1f5016869cc1c946bf810c54b00c0b2728094
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2021-11-10 23:19:35 -0500

    Add files via upload

---
+++ Client_RelayEcho.py
@@ -0,0 +1,51 @@
+# Client_RelayEcho.py
+# Billy Kihei (c) 2021
+# Intelligent Mobile Device Lab @ Kennesaw State University
+# Part of the 5Gtoolkit for testing commercial 5G networks.
+
+# This app measures the Layer 7 delay to send the character '0' to the UDP echo server.
+# The purpose of this app is to measure the Layer 7 round trip time from this phone to the echo server.
+
+# The intended use is to run this app in Termux.
+# Provide the number of times you would like to run this application.
+# Statistics will be returned to you.
+
+# 1. Open Termux.
+# 2. Download the 5gtoolkit git repo.
+# 3. Edit the config.json file so that server ip and port are correct.
+# 3. python Client_RelayEcho.py <number of measurements>
+
+import socket
+import sys
+import signal
+import time
+import json
+f = open('config.json',)
+conf = json.load(f)
+f.close()
+
+NumTimesToRun = sys.argv[1]
+
+pktnumber=0
+delays = []
+
+server_addr = (conf["echo_server"]["ip"], conf["echo_server"]["port"])
+
+udpClientSock= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def signal_handler(sig, frame):
+    udpClientSock.close()
+    print('\n')
+    print("%d bytes sent and received!\n" % (pktnumber))
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+print('Sending Packets...')
+while pktnumber!=NumTimesToRun:
+    udpClientSock.sendto(str.encode("0"), server_addr)
+    t = time.time()
+    data = udpClientSock.recvfrom(1024)
+    elapsed = time.time() - t
+    delays.append(elapsed)
+    pktnumber += 1

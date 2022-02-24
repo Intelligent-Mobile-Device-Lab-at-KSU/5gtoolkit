1a5cc1adfd2c6bab28385731150929d1f7ee1ab7
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2022-02-24 18:34:23 -0500

    Update EndPoint_UL_Goodput.py

--- icc/EndPoint_UL_Goodput.py
+++ icc/EndPoint_UL_Goodput.py
@@ -1,12 +1,12 @@
-# UploadGoodputRate.py
+# EndPoint_UL_Goodput.py
 # Billy Kihei (c) 2021
 # Intelligent Mobile Device Lab @ Kennesaw State University
 # Part of the 5Gtoolkit for testing commercial 5G networks.
 
-# This app measures the Layer 7 one-way Goodput from the phone to the UDP relay server.
-# The phone will send a message to the relay server asking to prepare for a one-way stream of bytes from the phone.
+# This app measures the Layer 7 one-way Goodput from the phone to the UDP EndPointServer.
+# The phone will send a message to the EndPointServer asking to prepare for a one-way stream of bytes from the phone.
 # The phone will stream the bytes according to the users desires.
-# The relay server will report the statistics.
+# The EndPointServer will report the statistics.
 
 # The intended use is to run this app in Termux.
 # Provide the size of each message, and the duration of the test in seconds.
@@ -14,7 +14,7 @@
 
 # 1. Open Termux.
 # 2. Download the 5gtoolkit git repo.
-# 3. Edit the config.json file so that relay server ip and port are correct.
+# 3. Edit the config.json file so that EndPointServer ip and port are correct.
 # 4. python UploadGoodputRate.py <size of packets in bytes> <duration of measurement in seconds>
 
 import socket
@@ -36,8 +36,8 @@ durationOfTestInSeconds = int(durationOfTestInSeconds_String)
 
 totalBytesSent = 0
 
-server_addr = (conf["relay_server"]["ip"], conf["relay_server"]["port"])
-server_halt_addr = (conf["relay_server"]["ip"], conf["relay_server"]["halt_port"])
+server_addr = (conf["endpoint_server"]["ip"], conf["endpoint_server"]["port"])
+server_halt_addr = (conf["endpoint_server"]["ip"], conf["endpoint_server"]["halt_port"])
 
 udpClientSock= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -50,15 +50,15 @@ def signal_handler(sig, frame):
         sys.exit(0)
     else:
         udpClientSock.close()
-        print('ERROR Could Not Stop Server \n')
+        print('ERROR Could Not Stop EndPointServer \n')
         sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 
-print('Sending UGR Request to Relay Server...')
-udpClientSock.sendto(str.encode("UGR:"+packetSizeInBytes_String), server_addr)
+print('Sending EUG Request to EndPointServer...')
+udpClientSock.sendto(str.encode("EUG:"+packetSizeInBytes_String), server_addr)
 respFromServer = udpClientSock.recvfrom(65507)
-print('Server ready.')
+print('EndPointServer ready.')
 print('Measurement in progress...')
 totalPacketsSent = 0
 t = time.time()
@@ -67,12 +67,12 @@ while (time.time() - t) <= durationOfTestInSeconds:
     udpClientSock.sendto(s.encode(), server_addr)
     totalPacketsSent += 1
 
-print("Done. Awaiting Stats From Relay Server...")
+print("Done. Awaiting Stats From EndPointServer...")
 udpClientSock.sendto(str.encode("done"), server_addr)
 respFromServer = udpClientSock.recvfrom(1024)
 stats = json.loads(respFromServer[0].decode())
 udpClientSock.close()
-print("Stats Received.")
+print("Endpoint UL Goodput Stats Received.")
 
 totalBytesRecvd = stats["totalBytesRecvd"]
 numberOfPackets = stats["numberOfPackets"]
@@ -81,9 +81,10 @@ goodput = totalBytesRecvd/durationOfTestInSeconds
 numberOfPackets = totalBytesRecvd/packetSizeInBytes
 packetReceptionRate = (numberOfPackets/totalPacketsSent)*100
 
-print("Phone Uploaded: " + str(totalPacketsSent) + " packets")
+print("==Endpoint UL Goodput==")
+print("Uploaded: " + str(totalPacketsSent) + " packets")
 print("Goodput (raw): %s bytes" % (goodput))
-print("Goodput (Megabitsps): %s " % ((goodput*8)/(1e6)))
+print("Goodput (Mbps): %s " % ((goodput*8)/(1e6)))
 print("Goodput (MegaBytesps): %s " % ((goodput)/(1e6)))
-print("Number of Packets Received by Server: " + str(numberOfPackets))
+print("Number of Packets Received by EndPointServer: " + str(numberOfPackets))
 print("Packet Reception Rate: " + str(packetReceptionRate) + "%")

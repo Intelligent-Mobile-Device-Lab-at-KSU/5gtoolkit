aa92ebe9a19228134424f364be4286cf701344d0
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2022-02-26 21:42:46 -0500

    Update HolePunchPeerRTT.py

--- icc/HolePunchPeerRTT.py
+++ icc/HolePunchPeerRTT.py
@@ -135,7 +135,7 @@ while (True):
         data = udpClientSock.recvfrom(pktsize)
         message = data[0]
         address = data[1]
-        print("Recevied from: %s, %s, s%" % (address[0], address[1],random.choice(string.digits) for _ in range(1)))
+        print("Recevied from: %s, %s, s%" % (address[0], address[1],''.join(random.choice(string.digits) for _ in range(pktsize))))
     except socket.timeout:
         print("Done.")
         udpClientSock.settimeout(None)

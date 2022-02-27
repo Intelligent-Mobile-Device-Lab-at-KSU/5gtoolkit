ea459e671a32a570511b7f5ed53dca189130c246
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2022-02-26 21:34:13 -0500

    Update HolePunchPeerRTT.py

--- icc/HolePunchPeerRTT.py
+++ icc/HolePunchPeerRTT.py
@@ -128,6 +128,14 @@ if username == 'b':
     udpClientSock.settimeout(None)
     udpClientSock.sendto(str.encode("READY"), peer_addr)
 
+udpClientSock.settimeout(5)
+print("Emptying buffer, please wait...")
+while (True):
+    try:
+        data = udpClientSock.recvfrom(pktsize)
+    except socket.timeout:
+        print("Done.")
+        udpClientSock.settimeout(None)
 print("Hole-Punch system ready.")
 # Device 1 should be logged into relay server as: a
 if username == 'a':
@@ -188,7 +196,10 @@ elif username == 'b':
     x=input("Press any key to receiving packets...")
     print('Listening for packets...')
     while True:
-        data, client_addr = udpClientSock.recvfrom(1024)
+        data, client_addr = udpClientSock.recvfrom(pktsize)
+        message = data[0]
+        address = data[1]
+        print("Recevied from: %s, %s" % (address[0], address[1]))
         if data.decode() == "peer_close":
             udpClientSock.close()
             break
